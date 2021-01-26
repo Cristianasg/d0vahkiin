11514af6f66777ea49bb37588d260fba44e7782f
Cristianasg <cristiangarciadasilva@gmail.com>
2021-01-26 18:24:28 -0300

    testes com array

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
 
-    echo "Hello World!";
+    $mes = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+    ];
+      
+    
+    $mes_selecionado = 2;
 
-
+    if ($mes_selecionado >= 1 && $mes_selecionado <= 12 ) {
+        echo $mes_selecionado;
+    }else{
+        echo "Mês é invalido!";
+    }
 ?>
