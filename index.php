35aec42f98cdbe718e721a99c8e7eb22f686c9c1
Cristianasg <cristiangarciadasilva@gmail.com>
2021-01-26 23:14:48 -0300

    exercicios logicos

--- index.php
+++ index.php
@@ -1,26 +1,49 @@
 <?php
 
-    $mes = [
-        "Janeiro",
-        "Fevereiro",
-        "Março",
-        "Abril",
-        "Maio",
-        "Junho",
-        "Julho",
-        "Agosto",
-        "Setembro",
-        "Outubro",
-        "Novembro",
-        "Dezembro",
-    ];
-      
-    
-    $mes_selecionado = 2;
+  //  $mes = [
+  //      "Janeiro",
+  //      "Fevereiro",
+  //      "Março",
+  //      "Abril",
+  //      "Maio",
+  //      "Junho",
+  //      "Julho",
+  //      "Agosto",
+  //      "Setembro",
+  //      "Outubro",
+  //      "Novembro",
+  //      "Dezembro"
+  //  ];
+  //    
+  //  echo $mes[1];
+  //  
+  // // $mes_selected = $mes[];
+  //  
+//
+  //  if ($mes >= 1 && $mes <= 12 ) {
+  //      echo $mes;
+  //  }else{
+ //       echo "Mês é invalido!";
+ //   }
 
-    if ($mes_selecionado >= 1 && $mes_selecionado <= 12 ) {
-        echo $mes_selecionado;
+    //$teste = 1;
+
+    //if(isset($teste)){
+    //    if ($teste == 1) {
+    //        for ($teste =1 ; $teste <= 100; $teste++) { 
+    //            $teste = $teste++;
+    //            echo $teste;
+    //        }
+    //    }
+    //   
+    //}    
+
+    $n = 5;
+    
+    if ($n % 2 != 0) {
+        echo "Par";
     }else{
-        echo "Mês é invalido!";
+        echo "Impar";
     }
+    
 ?>
